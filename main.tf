c6fd7403cc66aecddb5e68fb15d9acab5d8668e2
adadhore <107457695+adadhore@users.noreply.github.com>
2024-02-08 17:29:43 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,22 +4,6 @@ resource "aws_security_group" "Test_security_group" {
   name        = var.security_group
   description = "security group for Ec2 instance"
 
-  ingress {
-    from_port   = 0
-    to_port     = 65535
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-
- # outbound from jenkis server
-  egress {
-    from_port   = 0
-    to_port     = 65535
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
   tags= {
     Name = var.security_group
   }
